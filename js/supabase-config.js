7e15c01226fcfbcc8bd26d5fa3c065e553652158
2zoonh5 <2zoonh5@gmail.com>
2026-07-22 08:42:02 +0900

    Add Supabase configuration file

---
+++ js/supabase-config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://undmtjibnhdwevzfwcbi.supabase.co/rest/v1/';
+const SUPABASE_ANON_KEY = 'sb_publishable_wOiVdiHwZOW4JrS8eojDCw_AXMWS28b';
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
